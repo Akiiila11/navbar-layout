c19dfa12756ab1d44d598ed8f904613fb31f3d3b
Akiiila11 <akilavenkatraj2001@gmail.com>
2025-11-21 12:08:26 +0530

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <section className='w-full h-[50vh] flex items-center justify-center mt-20'> 
             <div className='font-bold text-center text-3xl'>
-                Home page - Search Component
+                Home page
             </div>  
         </section>
     );
